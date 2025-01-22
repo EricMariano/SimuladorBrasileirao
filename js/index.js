5a66d2ea7fdd83a5b1af457b77c4c79e353e8e95
EricMariano <ericbfmariano@gmail.com>
2025-01-22 19:01:02 -0300

    feature: Adição de troca de temas

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 //Logica
+const modeButton = document.getElementById('btnDarkMode')
+const body = document.body
 
+function changeMode() {
+    body.classList.toggle('dark-mode');
+    return body.classList.contains('dark-mode') ? modeButton.innerHTML = 'Modo Claro' : modeButton.innerHTML = 'Modo Dark';     
+}
+
+modeButton.addEventListener('click', changeMode);
